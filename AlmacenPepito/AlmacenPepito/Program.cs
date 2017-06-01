3a24add65e118c57403b39ffe02bcb51ad002600
Franco Zanek <francozanek@hotmail.com>
2017-06-01 19:45:38 -0300

    Creación del Proyecto

---
+++ AlmacenPepito/AlmacenPepito/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: franco
+ * Date: 1/6/2017
+ * Time: 19:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace AlmacenPepito
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
