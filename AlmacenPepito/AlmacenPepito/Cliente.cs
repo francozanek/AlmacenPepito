eb844161eee2b5b0f7af5d7aeff58c8a2aa453be
Franco Zanek <francozanek@hotmail.com>
2017-06-13 20:04:01 -0300

    Agregado de MovimientosHistoricos y Clientes

---
+++ AlmacenPepito/AlmacenPepito/Cliente.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: franco
+ * Date: 13/6/2017
+ * Time: 19:57
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace AlmacenPepito
+{
+	/// <summary>
+	/// Description of Cliente.
+	/// </summary>
+	public class Cliente
+	{
+		private int idCliente;
+		private string nombre;
+		
+		public Cliente(int idCliente, string nombre)
+		{
+			this.idCliente = idCliente;
+			this.nombre = nombre;
+		}
+		
+		public int manejarId{
+			get{return idCliente;}
+			set{idCliente = value;}
+		}
+		
+		public string manejarNombre{
+			get{ return nombre;}
+			set{ nombre =value;}
+		}
+	}
+}
